78b7e16addb72aae8a3ce711a025d6997f68b481
Joon Ho Cho <joonho1101@gmail.com>
2017-08-27 10:45:50 -0700

    Update RNMailComposePackage.java

--- android/src/main/java/com/reactlibrary/mailcompose/RNMailComposePackage.java
+++ android/src/main/java/com/reactlibrary/mailcompose/RNMailComposePackage.java
@@ -16,7 +16,7 @@ public class RNMailComposePackage implements ReactPackage {
       return Arrays.<NativeModule>asList(new RNMailComposeModule(reactContext));
     }
 
-    @Override
+    // Depreciated RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
       return Collections.emptyList();
     }
